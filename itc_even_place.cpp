693b233e5c1c6b3dcc8f78abb7f9365762720ab3
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-18 12:10:07 +0300

    Update itc_even_place.cpp

--- itc_even_place.cpp
+++ itc_even_place.cpp
@@ -2,9 +2,9 @@
 
 string itc_even_place(string str) {
     string ans;
-    for (int i = 0; i < itc_len(str); i++){
+    for (int i = 1; i =< itc_len(str); i++){
         if (i % 2 == 1)
-            ans += str[i];
+            ans += str[i - 1];
     }
     return ans;
 }
