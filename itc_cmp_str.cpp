be8376694ca8733a04596945f9c2e522f870afa9
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-18 12:13:17 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -4,8 +4,11 @@ string itc_cmp_str(string str1, string str2, int num) {
     string answer = "";
     for (int i = 0; i < num; i++)
         answer += str1[i];
-    for (int i = 0; str2[i] != '\0'; i++)
-        answer += str2[i];
+    int i = 0;
+    while (str2[i] != '\0'){
+         answer += str2[i];
+         i++;
+    }
     for (int i = num; str1[i] != '\0'; i++)
         answer += str1[i];
     return answer;
