3af041579600e0ceb5e4bbbf43ddfe9ca545e21d
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-18 12:43:51 +0300

    Update itc_even_place.cpp

--- itc_even_place.cpp
+++ itc_even_place.cpp
@@ -1,6 +1,8 @@
 #include "str_easy.h"
 
 string itc_even_place(string str) {
+    if (itc_len(str) < 2)
+        return "-1";
     string ans = "";
     for (int i = 1; i <= itc_len(str); i++){
         if (i % 2 == 1)
