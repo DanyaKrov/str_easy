0ef93158c41d5c3a04998f93f72648d0b89c865d
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-11 09:34:49 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -1,20 +1,18 @@
-#include "str_easy.h"
-
 string itc_cmp_str(string str1, string str2, int num) {
-    string answer = "";
+    string answer = ""; int kol = 0;
     int i1 = 0;
-    while (i1 < num){
+    while (i1 < num && kol != itc_len(str1)){
          answer += str1[i1];
-         i1++;
+         i1++; kol++;
     }
     int i = 0;
-    while (str2[i] != '\0'){
+    while (str2[i] != '\0' && kol != itc_len(str1)){
          answer += str2[i];
-         i++;
+         i++; kol++;
     }
-    while (str2[i1] != '\0'){
+    while (str2[i1] != '\0' && kol != itc_len(str1)){
          answer += str1[i1];
-         i1++;
+         i1++; kol++;
     }
     return answer;
 }
