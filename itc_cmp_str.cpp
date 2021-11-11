1f2b474589eabc29c7653ea194159529fd68ff2e
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-11 10:31:02 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -1,6 +1,8 @@
 #include "str_easy.h"
 
 string itc_cmp_str(string str1, string str2, int num) {
+    if (num < 0)
+        return "";
     string answer = ""; int kol = 0;
     int i1 = 0;
     while (i1 < num && kol != itc_len(str1)){
